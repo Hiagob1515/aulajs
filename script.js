0ac412fbd4f8d0317d081eab975e62668643a65a
Hiago Batista <hiago.elit@gmail.com>
2025-08-31 17:15:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function adicionarTarefa(){
+    
+    
+    const inputTarefa = document.getElementById("inputTarefa")
+    const tarefa = inputTarefa.value.trim()
+    const mensagem = document.getElementById("mensagem")
+
+     // se o valor do input for vazio entao mostre uma mensagem de erro para o usuario
+    if (tarefa == "") {
+        //mostre uma mensagem de erro
+       const mensagemErro =  "Digite Uma Tarefa Para Adiciona-la A Sua Lista!";
+       mensagem.textContent = mensagemErro;
+       mensagem.style.color = "#A34743"
+    } else{
+       const mensagemSucesso =  "Tarefa Adicionada Com Sucesso!";
+       mensagem.textContent = mensagemSucesso;
+       mensa.style.color = "#28A745"
+    const listaTarefas = document.getElementById("listaTarefas")
+    const novaTarefa = document.createElement("li")
+    novaTarefa.textContent = tarefa
+    listaTarefas.appendChild(novaTarefa)
+    }  
+
+    
+
+   
+    
+   
+    inputTarefa.value = ""
+    
+
+
+
+}
